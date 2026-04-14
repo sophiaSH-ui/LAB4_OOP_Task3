85886e3aa08e9345b7c9bcbee42300253f4f03da
Артем Плаха <artemplakha@gmail.com>
2026-04-14 12:38:15 +0300

    added the DB class

---
+++ lab4_task3/DTO.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Npgsql;
+
+namespace lab4_task3
+{
+    string connectionString = "Host=78.137.52.42;Username=socksof;Password=вуафгде;Database=properties";
+
+    class DB
+    {
+        public ObservableCollection<Property> GetProperties(Locality locality = null)
+        {
+            using var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            var owners = new Dictionary<int, Owner>();
+
+            using var command = new NpgsqlCommand();
+            command.Connection = connection;
+
+            if (locality is null)
+            {
+                command.CommandText = "SELECT id, name, surname, birthdate FROM properties;";
+            }
+            else
+            {
+                command.CommandText = "SELECT id, name, surname, birthdate FROM properties WHERE locality = @localityId;";
+
+                command.Parameters.AddWithValue("localityId", locality.Id);
+            }
+
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int ownerID = reader.GetInt32(reader.GetOrdinal("id"));
+
+                var owner = new Owner(
+                    reader.GetString(reader.GetOrdinal("name")),
+                    reader.GetString(reader.GetOrdinal("surname")),
+                    reader.GetDateTime(reader.GetOrdinal("birthdate"))
+                );
+
+                owners[ownerID] = owner;
+            }
+        }
+
+        public int GetPropertiesCount(Locality locality = null)
+        {
+            using var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            using var command = new NpgsqlCommand();
+            command.Connection = connection;
+
+            if (locality is null)
+            {
+                command.CommandText = "SELECT COUNT(*) FROM properties;";
+            }
+            else
+            {
+                command.CommandText = "SELECT COUNT(*) FROM properties WHERE locality = @localityId;";
+
+                command.Parameters.AddWithValue("localityId", locality.Id);
+            }
+
+            long count = (long)command.ExecuteScalar();
+
+            return count;
+        }
+    }
+
+    class Owner
+    {
+        
+    }
+
+    class Property
+    {
+
+    }
+
+    class Description
+    {
+
+    }
+
+    class Locality
+    {
+
+    }
+}
--- lab4_task3/lab4_task3/obj/Release/net9.0-windows/lab4_task3.AssemblyInfo.cs
+++ lab4_task3/lab4_task3/obj/Release/net9.0-windows/lab4_task3.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4_task3")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28f18079e29b948b26532920331f37b6b240c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4_task3")]
